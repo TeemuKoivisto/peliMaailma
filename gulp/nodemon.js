6f199af48d17513a3f7ea6568094c484e8b5f136
TeemuKoivisto <tkoivisto456@gmail.com>
2015-11-22 13:30:36 +0200

    fix some spacing

--- gulp/nodemon.js
+++ gulp/nodemon.js
@@ -2,10 +2,10 @@ var gulp = require('gulp'),
     nodemon = require('gulp-nodemon');
     
 gulp.task('nodemon', ['watch-client'], function() {
-	nodemon({
-		script: 'index.js',
-		ext: 'js html css',
-		ignore: ['client/app/**/*.js', 'gulp', 'test', 'node_modules', 'client/bower_components']
-	})
+    nodemon({
+        script: 'index.js',
+        ext: 'js html css',
+        ignore: ['client/app/**/*.js', 'gulp', 'test', 'node_modules', 'client/bower_components']
+    })
 })
 
